84b1a17492aec97e73f43c0879c8620caedf0e67
fgwsz <3191000458@qq.com>
2026-01-02 18:38:24 +0800

    add FC sav

--- run-fc.sh
+++ run-fc.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 root_path=$(dirname "$(readlink -f "$0")")
+git_fc_fcs="$root_path/FC/fcs/"
+git_fc_sav="$root_path/FC/sav/"
+sys_fc_fcs=~/.fceux/fcs/
+sys_fc_sav=~/.fceux/sav/
+
 cd "$root_path"
 git fetch origin
 git reset --hard origin/main
-cp -rf ./FC/cfs/* ~/.fceux/cfs/
-cp -rf ./FC/sav/* ~/.fceux/sav/
+rsync -avzh --delete "$git_fc_fcs" "$sys_fc_fcs"
+rsync -avzh --delete "$git_fc_sav" "$sys_fc_sav"
 fceux
-cp -rf ~/.fceux/cfs/* ./FC/cfs/
-cp -rf ~/.fceux/sav/* ./FC/sav/
+rsync -avzh --delete "$sys_fc_fcs" "$git_fc_fcs"
+rsync -avzh --delete "$sys_fc_sav" "$git_fc_sav"
 git status
 git add -A
 git commit -m "Push game sav."
