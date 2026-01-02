758399aba707f3c6601bc5d2d36a0ad08d5b2647
fgwsz <3191000458@qq.com>
2026-01-02 18:13:48 +0800

    impl fc sav

--- run-fc.sh
+++ run-fc.sh
@@ -4,7 +4,11 @@ root_path=$(dirname "$(readlink -f "$0")")
 cd "$root_path"
 git fetch origin
 git reset --hard origin/main
+cp -rf ./FC/cfs/* ~/.fceux/cfs/
+cp -rf ./FC/sav/* ~/.fceux/sav/
 fceux
+cp -rf ~/.fceux/cfs/* ./FC/cfs/
+cp -rf ~/.fceux/sav/* ./FC/sav/
 git status
 git add -A
 git commit -m "Push game sav."
